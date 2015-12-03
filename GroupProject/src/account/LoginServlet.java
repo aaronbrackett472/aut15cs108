ae762ed9ba9a46540502a47ffdc5c45f19f145d6
Eric Musyoka <ericmutua100@gmail.com>
2015-12-02 21:34:17 -0800

    User.java changed, UserTest.java added, UserSessionListener removed; minore edits CreateAccountServlet an LoginServlet

--- GroupProject/src/account/LoginServlet.java
+++ GroupProject/src/account/LoginServlet.java
@@ -3,22 +3,15 @@ package account;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import database.DatabaseConnection;
-
 import database.DBContextListener;
-import database.DatabaseConnection;
 
-import database.DBContextListener;
-import database.DatabaseConnection;
 
 /**
  * Servlet implementation class LoginServlet
--- GroupProject/src/account/UserTest.java
+++ GroupProject/src/account/UserTest.java
@@ -1,15 +1,49 @@
-//package QuizProject;
+
 package account;
 
+import database.*;
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import java.util.*;
 
 public class UserTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void addFriendTest() {
+		DatabaseConnection connection =  new DatabaseConnection();
+		AccountManager accounts =  new AccountManager(connection);
 
+		//add users
+		accounts.registerUser("amuj", "isokum");
+		accounts.registerUser("juma", "mukoi");
+		accounts.registerUser("uvyu", "");
+		
+		//make friends
+		User user =  new User("amuj", connection);
+		user.addFriend("juma");
+		user.addFriend("kish");
+		user.addFriend("uvyu");
+		
+		//test
+		assertEquals(false, user.checkFriend("amuj"));
+		assertEquals(true, user.checkFriend("juma"));
+		assertEquals(false, user.checkFriend("kish"));
+		assertEquals(true, user.checkFriend("uvyu"));
+		
+		//bi-directionality of friendships
+		User user2 = new User("uvyu", connection);
+		assertEquals(true, user2.checkFriend("amuj"));
+		connection.close();
+	}
+	
+	@Test
+	public void getAllFriendsTest() {
+		DatabaseConnection connection =  new DatabaseConnection();
+		Set<String> amujFriends =  new HashSet<String>(Arrays.asList("juma", "uvyu"));
+		User userAmuj =  new User("amuj", connection);
+		assertEquals(true, userAmuj.getAllFriends().equals(amujFriends));
+		connection.close();
+			
+	}
+	
 }
