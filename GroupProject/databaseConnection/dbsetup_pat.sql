10897f5700f363d24f2900fb17f706d9231bbd24
patboony <patboony@gmail.com>
2015-12-02 16:26:10 -0800

    minor sql schema update - accounts table

--- GroupProject/databaseConnection/dbsetup_pat.sql
+++ GroupProject/databaseConnection/dbsetup_pat.sql
@@ -2,15 +2,18 @@ use c_cs108_mateog;
 
 DROP TABLE IF EXISTS Choices;
 DROP TABLE IF EXISTS Questions;
-DROP TABLE IF EXISTS Quizzes;
 DROP TABLE IF EXISTS History;
+DROP TABLE IF EXISTS Quizzes;
 DROP TABLE IF EXISTS Achievement;
 DROP TABLE IF EXISTS Friendship;
 DROP TABLE IF EXISTS Accounts;
 
 CREATE TABLE Accounts (
 	username VARCHAR(64) NOT NULL PRIMARY KEY,
-	password VARCHAR(64) NOT NULL
+	password VARCHAR(64) NOT NULL,
+	isAdmin BOOL NOT NULL DEFAULT 0,
+	suspended BOOL NOT NULL DEFAULT 0,
+	suspensionEnd TIMESTAMP
 );
 
 CREATE TABLE Friendship (
@@ -24,17 +27,9 @@ CREATE TABLE Achievement (
 	username VARCHAR(64),
 	FOREIGN KEY (username) REFERENCES Accounts(username),
 	achivementName VARCHAR(64),
-	timeAcquired VARCHAR(64)
-);
-
-CREATE TABLE History (
-	username VARCHAR(64),
-	FOREIGN KEY (username) REFERENCES Accounts(username),
-	score INT,
-	maxScore INT,
-	quizId INT NOT NULL,
-	FOREIGN KEY (quizId) REFERENCES Quizzes(id),
-	dateTaken TIMESTAMP
+	achivementDescription VARCHAR(255),
+	image VARCHAR(255),
+	dateAcquired TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Quizzes (
@@ -48,9 +43,18 @@ CREATE TABLE Quizzes (
      createdBy VARCHAR(64) NOT NULL,
      FOREIGN KEY (createdBy) REFERENCES Accounts(username),
      description TEXT NOT NULL DEFAULT '',
-     createdDate TIMESTAMP
+     createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE History (
+	username VARCHAR(64) NOT NULL,
+	FOREIGN KEY (username) REFERENCES Accounts(username),
+	score INT,
+	maxScore INT,
+	quizId INT NOT NULL,
+	FOREIGN KEY (quizId) REFERENCES Quizzes(id),
+	dateTaken TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Represents all questions in the website
 CREATE TABLE Questions (
@@ -75,8 +79,8 @@ CREATE TABLE Choices (
 );
 
 -- Sample user
-INSERT INTO Accounts (username, password) VALUES("pat", "patpatpat");
-INSERT INTO Accounts (username, password) VALUES("eric", "1234");
+INSERT INTO Accounts (username, password, isAdmin, suspended) VALUES("pat", "patpatpat", 0, 0);
+INSERT INTO Accounts (username, password, isAdmin, suspended) VALUES("eric", "1234", 0, 0);
 
 -- Sample quiz
 INSERT INTO Quizzes (name, randomOrder, singlePage, immediateCorrection, practiceModeAllowed, takenCounter, createdBy, description, createdDate) VALUES ("TV Shows Trivia", 0, 1, 0, 0, 0, "pat", "A must-take quiz if you think you know more about TV shows than any of your friends", "2015-11-23 06:15:00");
